418ce16b6943644447ba5a2634ca270ac3bec578
Shameel Kadannamanna <shameelsadaka@gmail.com>
2018-11-24 01:38:17 +0530

    Attendance Structure

--- attendance/urls.py
+++ attendance/urls.py
@@ -4,7 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index,name='index'),
+    path('unit', views.index),
     path('unit/<int:unit_id>', views.unit),
     path('unit/<int:unit_id>/new/', views.new),
+    path('student/<int:student_id>', views.student),
+    path('unit/<int:unit_id>/view/', views.attendances),
     path('view/<int:attendance_id>', views.view),
 ]
--- attendance/views.py
+++ attendance/views.py
@@ -15,11 +15,23 @@ def new(request,unit_id):
 	return render(request, 'attendancesheet.html', {'output': output})
 
 
-def view(request,unit_id):
+def view(request,attendance_id):
+
+	output = Students.objects.filter(unit=unit_id);
+	return render(request, 'attendancesheet.html', {'output': output})
+
+
+def attendances(request,unit_id):
 
 	output = Students.objects.filter(unit=unit_id);
 	return render(request, 'attendancesheet.html', {'output': output})
 
 def unit(request,unit_id):
-	output = Students.objects.filter(unit=unit_id);
-	return render(request, 'attendancesheet.html', {'output': output})
+	students = Students.objects.filter(unit=unit_id);
+	thisunit = NSSUnits.objects.get(id=unit_id);
+	return render(request, 'unit.html', {'students': students,'unit':thisunit})
+
+
+def student(request,student_id):
+	output = Students.objects.get(id=student_id);
+	return render(request, 'student.html', {'output': output})
