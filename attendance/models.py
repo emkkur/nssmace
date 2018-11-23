94ce596a80a13dcd2cf837c58db35534db2f6d48
Emil K Kurian <emkurian97@gmail.com>
2018-11-23 20:42:55 +0530

    Attendance start

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+class NSSUnits(models.Model):
+    name = models.CharField(max_length=30)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
+class Students(models.Model):
+    name = models.CharField(max_length=30)
+    adno = models.CharField(max_length=30)
+    contact_number = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1,choices=genderchoice)
+    dob = models.CharField(max_length=10)
+    unit_number = models.CharField(max_length=3)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
+
